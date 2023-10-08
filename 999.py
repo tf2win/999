3bc0e5555ac15afa1cdaa95325ca8775d0f792e6
dr volt <43712231+tf2win@users.noreply.github.com>
2023-10-08 17:53:46 +0000

    Update 999.py 8.okt 2023
    This is starting to work but when voltage is applied to pin 38, nothing happens, need to reset

--- 999.py
+++ 999.py
@@ -1,19 +1,28 @@
+# sudo apt-get install python 
+# sudo apt-get update
+# sudo apt-get install rpi.gpio
+# sudo apt-get install -y python-requests
+# sudo apt-get install python time
+# sudo pip install requests
+# sudo pip3 install requests
+# sudo apt-get upgrade
+# python3 (run program)
 #!/usr/bin/python
-import datetime
+# -*- coding: utf-8 -*-
 import json
 import time
-import requests
-import csv
 import math
-# Uppsetning á seven segment skjá:
-# code modified, tweaked and tailored from code by bertwert
-# on RPi forum thread topic 91796
+import requests
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+receive_pin = 38
+GPIO.setup(receive_pin, GPIO.IN)
+
 # GPIO ports for the 7seg pins
-segments =  (11,4,23,8,7,10,18,25)
+segments =  (11, 4, 23, 8, 7, 10, 18, 25)
 # 7seg_segment_pins (11,7,4,2,1,10,5,3) +  100R inline
 
 for segment in segments:
@@ -21,9 +30,8 @@ for segment in segments:
     GPIO.output(segment, 0)
 
 # GPIO ports for the digit 0-3 pins
-digits = (22,27,17,24)
+digits = (22, 27, 17, 24)
 # 7seg_digit_pins (12,9,8,6) digits 0-3 respectively
-
 for digit in digits:
     GPIO.setup(digit, GPIO.OUT)
     GPIO.output(digit, 1)
@@ -39,104 +47,89 @@ num = {' ':(0,0,0,0,0,0,0),
     '7':(1,1,1,0,0,0,0),
     '8':(1,1,1,1,1,1,1),
     '9':(1,1,1,1,0,1,1)}
-# Búið að setja upp seven segment skjá
-# pip install requests
 LANDSNET_DATA = None
+
 def save_to_json(filepath, data, pretty=False):
-    '''
-    takes in filepath and data object saves data to json in filepath
-    '''
     if pretty:
-        data_text = json.dumps(data, indent=4, ensure_ascii=False,sort_keys=True)
+        data_text = json.dumps(data, indent=4, ensure_ascii=False, sort_keys=True)
     else:
-        data_text = json.dumps(data, separators=(',', ':'),ensure_ascii=False, sort_keys=True)
-    with open(filepath, mode='w', encoding='utf-8') as json_file:json_file.write(data_text)
+        data_text = json.dumps(data, separators=(',', ':'), ensure_ascii=False, sort_keys=True)
+    with open(filepath, mode='w', encoding='utf-8') as json_file:
+        json_file.write(data_text)
+
 def fetch_landsnet_data():
-    time.sleep(1)
-    landsnet_page ='https:///'
-    url = 'https://'
+    landsnet_page = 'ASK AUTHORS!'
+    url = 'ASK AUTHORS!'
     res = requests.get(url)
     data = res.json()
     better_data = {}
     for measurement in data:
         better_data[measurement['key']] = measurement
     save_to_json('better_data.json', better_data, pretty=True)
-
-    gogn1 = (better_data['TA1']['MW']) # velja gögn til að birtal
-    gogn2 = (better_data['TA1']['time'])
-
+    gogn1 = better_data['TA1']['MW']
+    gogn2 = better_data['TA1']['time']
     print("hallo heimur")
     print(gogn1)
     print(gogn2)
-
-        # Stærðargráða tölunnar fundin (hvar komman er)
-    if gogn1 == 0:
+    
+    if gogn1 == 0:    
         staerdargrada_gogn1 = 0
     else:
         staerdargrada_gogn1 = int(math.floor(math.log10(abs(gogn1))))
     if gogn1 < 0:
-        gogn1_formatted == gogn1
+        gogn1_formatted = gogn1
     else:
-        gogn1_formatted = gogn1/pow(10,staerdargrada_gogn1)*1000 # Talan sett á staðlað form
+        gogn1_formatted = gogn1 / pow(10, staerdargrada_gogn1) * 1000
     # Seven segment keyrsla:
-
     try:
         while True:
-        #    n = time.ctime()[11:13]+time.ctime()[14:16]
-        #    s = str(n).rjust(4)
-            # a = int(gogn3*1000)
             if gogn1_formatted == 0:
                 a = 0
             else:
                 a = int(gogn1_formatted)
-
             #  print(a)
-            s = str(a)  # Hér seturðu inn töluna sem þú vilt birta.
-
-            if staerdargrada_gogn1 > 3:  # Ef talan er stærri en 9999 þá birtist 9999 á skjánum
+            s = str(a)
+            if staerdargrada_gogn1 > 3:
                 s = '9999'
-            elif staerdargrada_gogn1 == -1: # Ef talan er núll komma eitthvað þá bætist 0 framan á strenginn
+            elif staerdargrada_gogn1 == -1:
                 s = '0' + s
-            elif staerdargrada_gogn1 == -2: # Ef talan byrjar á 0.0 bætist 00 framan á strenginn
+            elif staerdargrada_gogn1 == -2:
                 s = '0' + '0' + s
-            elif staerdargrada_gogn1 == -3: # Ef talan byrjar á 0.00 bætist 000 framan á strenginn
+            elif staerdargrada_gogn1 == -3:
                 s = '0' + '0' + '0' + s
-            elif staerdargrada_gogn1 < -3: # Ef talan byrjar á 0.000 þá birtist 0000 á skjánum
+            elif staerdargrada_gogn1 < -3:
                 s = '0000'
             for digit in range(4):
                 for loop in range(0,7):
                     GPIO.output(segments[loop], num[s[digit]][loop])
-# Hér kemur á dislpaly
-                    #if (int(time.ctime()[18:19])%2 == 0) and (digit == 1): # Hér er staðeing puntins.gp
-                    #    GPIO.output(25, 1)
-                    #else:
-                    #    GPIO.output(25, 0)
-                    if -3 <= staerdargrada_gogn1 < 1 and digit == 0: # Staðsetning punkts fyrir litla tölu
+                    if -3 <= staerdargrada_gogn1 < 1 and digit == 0:
                         GPIO.output(25,1)
-                    elif staerdargrada_gogn1 == 1 and digit == 1: # Staðsetning punkts fyrir tugatölu
+                    elif staerdargrada_gogn1 == 1 and digit == 1:
                         GPIO.output(25,1)
-                    elif staerdargrada_gogn1 == 2 and digit == 2: # Staðsetning punkts fyrir hundruð
+                    elif staerdargrada_gogn1 == 2 and digit == 2:
                         GPIO.output(25,1)
                 GPIO.output(digits[digit], 0)
-                time.sleep(0.001) # Skiptir á milli dig hraðin 
+                time.sleep(0.005)
                 GPIO.output(digits[digit], 1)
                 GPIO.output(25,0)
-
+                if check_voltage():
+                       print("Spenna kom á pinna 38! Endurrótum forritið...")
+                       time.sleep(0.01)  # Bíða í 100ms
+                if check_voltage():
+                    return  # Hætta í loopu ef spenna er kominn aftur
+                
     finally:
-
         GPIO.cleanup()
-# kÓÐIN FYRIR NEÐAN ER AÐ HANN ENDURHLEÐUR EFTIR 50000000000sek
+
+def check_voltage():
+    return GPIO.input(receive_pin) == GPIO.HIGH
+
 def main():
-    data_timestamp = time.time_ns()
-    data = fetch_landsnet_data()
-    temp = None
     while True:
-        current_timestamp = time.time_ns()
-        data_age = current_timestamp - data_timestamp
-        if (data_age > 400000):  # nanoseconds
-            print('reloading data')
-            data_timestamp = time.time_ns()
-            data = fetch_landsnet_data()
+        fetch_landsnet_data()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        GPIO.cleanup()
